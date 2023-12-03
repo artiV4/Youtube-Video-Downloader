6fe1ebc2108cab77967bfa2982d61df556512ade
Artemiy Vishnyakov <122134009+artiV4@users.noreply.github.com>
2023-12-02 22:16:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def downloadVideo(link):
         print(e)
         print("Invalid link")
         exit(-1)
-    print("downloading "+video.title + " as " + ("audio" if audio else "video"))
+    print("downloading "+link + " as " + ("audio" if audio else "video"))
     if audio: video = video.streams.get_audio_only()
     else: video = video.streams.get_highest_resolution()
 
@@ -21,11 +21,43 @@ def downloadVideo(link):
     except Exception as e:
         print("Error occurred when trying to download")
         print(e)
-        exit(-1)
+        # exit(-1)
 
     print("Download ended")
 
+def download2(link):
+    try:
+         print("downloading "+link)
+         video = YouTube(link).streams.get_audio_only()
 
-audio = False
-link = "Your Link Here"
-downloadVideo(link)
+    except Exception as e:
+        print(e)
+        print("Invalid link:\t\t"+link)
+        
+    try:
+        name = video.default_filename  # default suffix is .mp4, even for audio only
+        if audio:
+            split = name.split('.')
+            name = split[0] + ".mp3"
+        video.download(filename=name)
+    except Exception as e:
+        print("Error occurred when trying to download")
+        print(e)
+
+
+audio = True
+fromfile = True
+
+links=[]
+
+if (fromfile):
+    with open("linksfile.txt","r") as readfile:
+        for line in readfile:
+            links.append(line.rstrip('\n').split('&')[0])
+
+else:
+    links = ["",
+            ""
+         ]
+for link in links:
+    download2(link)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+A program to download videos from youtube, paste links into linksfile.txt, 1 link per line and then run main.py.
+
+By deafult downloads them as audio only since that is what I used it for
